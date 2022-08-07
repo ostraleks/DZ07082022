8b0f0ed364a950448bc71e54307834f198282023
aleksndr <ostraleks@gmail.com>
2022-08-07 22:42:06 +0500

    3d

---
+++ 2/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6*/
+
+string inputNumber = Console.ReadLine();
+int length = inputNumber.Length;
+if (length>2)
+{
+    Console.WriteLine($"Третья цифра: {inputNumber[2]}");
+}
+else Console.WriteLine($"Третьей цифры нет");
