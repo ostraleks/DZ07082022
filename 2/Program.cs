57869095184f2cf88c5331457bd19ce0029fe4a1
aleksndr <ostraleks@gmail.com>
2022-08-08 14:32:57 +0500

    #2

--- 2/Program.cs
+++ 2/Program.cs
@@ -4,7 +4,7 @@
 
 78 -> третьей цифры нет
 
-32679 -> 6*/
+32679 -> 6
 
 Console.Write("Введите число ");
 string inputNumber = Console.ReadLine();
@@ -13,4 +13,19 @@ if (length>2)
 {
     Console.WriteLine($"Третья цифра: {inputNumber[2]}");
 }
-else Console.WriteLine($"Третьей цифры нет");
+else Console.WriteLine($"Третьей цифры нет");*/
+
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100) Console.WriteLine("Третьей цифры нет");
+else
+if (number > 99 && number < 1000) Console.WriteLine($"Третья цифра  {number % 100 % 10}");
+else
+{
+    while (number >= 1000)
+{
+    number = number / 10;
+}
+Console.WriteLine($"Третья цифра  {number % 100 % 10}");
+}
+
